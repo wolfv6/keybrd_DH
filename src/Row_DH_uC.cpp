9ff65b033381787854be6ce04a4499f7e107b9b9
wolfv6 <wolfv6@users.noreply.github.com>
2016-07-08 01:05:29 -0600

    remove Row_IOE

--- src/Row_DH_uC.cpp
+++ src/Row_DH_uC.cpp
@@ -5,9 +5,9 @@ const bool RowScanner_PinsArray::ACTIVE_HIGH = 1;
 void Row_DH_uC::process()
 {
     //these variables are all bitwise, one bit per key
-    read_pins_t rowState;                       //1 means pressed, 0 means released
-    read_pins_mask_t rowEnd;                    //1 bit marks positioned after last key of row
-    read_pins_t debouncedChanged;               //1 means debounced changed
+    uint8_t rowState;                       //1 means pressed, 0 means released
+    uint8_t rowEnd;                    //1 bit marks positioned after last key of row
+    uint8_t debouncedChanged;               //1 means debounced changed
 
     rowState = scanner.scan(rowEnd);
     debouncedChanged = debouncer.debounce(rowState, debounced);
