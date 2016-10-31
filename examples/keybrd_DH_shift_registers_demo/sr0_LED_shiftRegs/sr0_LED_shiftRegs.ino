5ee4658e5642504fba7c94a88879f051820236b7
wolfv6 <wolfv6@users.noreply.github.com>
2016-10-31 17:43:44 -0600

    add tri-state and right 74HC165 to sr0_LED_shiftRegs.ino

--- examples/keybrd_DH_shift_registers_demo/sr0_LED_shiftRegs/sr0_LED_shiftRegs.ino
+++ examples/keybrd_DH_shift_registers_demo/sr0_LED_shiftRegs/sr0_LED_shiftRegs.ino
@@ -6,7 +6,7 @@ This sketch:
 
 | Left  | **0** | **1** | **2** |
 |-------|-------|-------|-------|
-| **0** |CapsLck|  fn   | a  1  |
+| **0** |  fn   |CapsLck| a  1  |
 */
 // ################## GLOBAL ###################
 // ================= INCLUDES ==================
@@ -31,16 +31,19 @@ ScanDelay scanDelay(9000);
 
 // ==================== ICs ====================
 // ---------------- LEFT SCANNER ---------------
-Scanner_ShiftRegsPISOMultiRow scanner_L(HIGH, 6, 1); //active HIGH, SS pin 6, 1 byte
+Scanner_ShiftRegsPISOMultiRow scanner_L(HIGH, 6, 1); //active HIGH, slave select 6, 1 byte
 
 // ----------------- LEFT LED ------------------
 LED_uC LED_left(4);                             //pin 4
 
-// ---------------- RIGHT LEDs -----------------
+// ---------------- RIGHT SCANNER --------------
+Scanner_ShiftRegsPISOMultiRow scanner_R(HIGH, 7, 1); //active HIGH, slave select 7, 1 byte
+
+// ----------------- RIGHT LEDs ----------------
 Port_ShiftRegs shiftRegs(8);                    //slave select 8
 
-LED_Port LED_rightTop(shiftRegs, 1<<6);     //pin 6
-LED_Port LED_rightBot(shiftRegs, 1<<7);     //pin 7
+LED_Port LED_rightTop(shiftRegs, 1<<6);         //pin 6
+LED_Port LED_rightBot(shiftRegs, 1<<7);         //pin 7
 
 // =================== CODES ===================
 // ---------------- LAYER CODE -----------------
@@ -54,19 +57,24 @@ Code_LayerHold l_fn(FN, layerState);
 // ---------------- SCAN CODES -----------------
 Code_Sc s_a(KEY_A);
 Code_Sc s_x(KEY_X);
+Code_Sc s_y(KEY_Y);
 Code_Sc s_1(KEY_1);
 Code_LEDLock o_capsLock(KEY_CAPS_LOCK, LED_rightBot);
 
 // =================== KEYS ====================
-Key* const ptrsKeys_01[] = { &s_a, &s_1 };
-Key_LayeredKeys k_01(ptrsKeys_01);
+Key* const ptrsKeys_a[] = { &s_a, &s_1 };
+Key_LayeredKeys k_a(ptrsKeys_a);
 
 LayerStateInterface& Key_LayeredKeys::refLayerState = layerState;
 
-// ==================== ROW ====================
-Key* ptrsKeys_0[] = { &s_x, &o_capsLock, &l_fn, &k_01 };
-const uint8_t KEY_COUNT_0 = sizeof(ptrsKeys_0)/sizeof(*ptrsKeys_0);
-Row row_L(scanner_L, 0, ptrsKeys_0, KEY_COUNT_0); //strobe pin 0
+// ==================== ROWS ===================
+Key* ptrsKeys_L[] = { &s_x, &l_fn, &o_capsLock };
+const uint8_t KEY_COUNT_0 = sizeof(ptrsKeys_L)/sizeof(*ptrsKeys_L);
+Row row_L(scanner_L, 0, ptrsKeys_L, KEY_COUNT_0); //strobe pin 0
+
+Key* ptrsKeys_R[] = { &s_y, &k_a };
+const uint8_t KEY_COUNT_1 = sizeof(ptrsKeys_R)/sizeof(*ptrsKeys_R);
+Row row_R(scanner_R, 1, ptrsKeys_R, KEY_COUNT_1); //strobe pin 1
 
 /* ################### MAIN ####################
 LayerState_LED::begin() is called after Scanner_IOE::begin()
@@ -78,12 +86,14 @@ void setup()
     delay(6000);
     Keyboard.print(" in setup() ");
     scanner_L.begin();
+    scanner_R.begin();
     layerState.begin();                 //call LayerState_LED::begin() after Scanner::begin()
 }
 
 void loop()
 {
     row_L.process();
+    row_R.process();
 
     scanDelay.delay();
     //debug.print_scans_per_second();
