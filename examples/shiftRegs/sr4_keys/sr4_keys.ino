e37362aa7c2c41503da1124960106d8bd7f9f1c1
wolfv6 <wolfv6@users.noreply.github.com>
2016-11-02 20:15:30 -0600

    add LEDs to sr4_keys.ino, add breadboard pictures

--- examples/shiftRegs/sr4_keys/sr4_keys.ino
+++ examples/shiftRegs/sr4_keys/sr4_keys.ino
@@ -1,23 +1,27 @@
 /* sr4_keys.ino
 
-This sketch is a simple 2-layer keyboard with two rows of PISO shift registers
+This sketch is a simple 2-layer keyboard with two rows of PISO shift registers.
+Indicator two LEDs are on POSI shift registers.
 
 | Left  | **0** | **1** | | Right | **0** | **1** |
 |-------|-------|-------| |-------|-------|-------|
-| **1** |   l   |   6   | | **1** |   r   |   7   |
+| **1** |  fn   | a   1 | | **1** |   b   |   c   |
 */
 // ################## GLOBAL ###################
 // ================= INCLUDES ==================
 #include <ScanDelay.h>
 #include <LayerState_LED.h>
 #include <Code_LayerHold.h>
-#include <Code_LEDLock.h>
 #include <Key_LayeredKeys.h>
 #include <Code_Sc.h>
 #include <Row.h>
 
 //scanner
 #include <Scanner_ShiftRegsReadStrobed.h>
+
+//LEDs
+#include <Port_ShiftRegs.h>
+#include <LED_Port.h>
 
 // ============ SPEED CONFIGURATION ============
 ScanDelay scanDelay(9000);
@@ -29,23 +33,43 @@ Scanner_ShiftRegsReadStrobed scanner_L(HIGH, 6, 1); //active HIGH, slave select 
 // ---------------- RIGHT SCANNER --------------
 Scanner_ShiftRegsReadStrobed scanner_R(HIGH, 7, 1); //active HIGH, slave select 7, 1 byte
 
+// ----------------- RIGHT LEDs ----------------
+Port_ShiftRegs shiftRegs(8);                    //slave select 8
+
+LED_Port LED_top(shiftRegs, 1<<6);              //pin 6
+LED_Port LED_bot(shiftRegs, 1<<7);              //pin 7
+
 // =================== CODES ===================
+// ---------------- LAYER CODE -----------------
+enum layerIds { NORMAL, FN };
+
+LEDInterface* prtsLayerLEDs[] = { &LED_top, &LED_bot };
+LayerState_LED layerState(prtsLayerLEDs);
+
+Code_LayerHold l_fn(FN, layerState);
+
 // ---------------- SCAN CODES -----------------
-Code_Sc s_L(KEY_L);
-Code_Sc s_R(KEY_R);
+Code_Sc s_A(KEY_A);
+Code_Sc s_B(KEY_B);
+Code_Sc s_C(KEY_C);
 Code_Sc s_X(KEY_X);
 Code_Sc s_Y(KEY_Y);
-Code_Sc s_6(KEY_6);
-Code_Sc s_7(KEY_7);
+Code_Sc s_1(KEY_1);
+
+// =================== KEYS ====================
+Key* const ptrsKeys_a[] = { &s_A, &s_1 };
+Key_LayeredKeys k_a(ptrsKeys_a);
+
+LayerStateInterface& Key_LayeredKeys::refLayerState = layerState;
 
 // ==================== ROWS ===================
-Key* ptrsKeys_L[] = { &s_X, &s_L, &s_6 };
-const uint8_t KEY_COUNT_0 = sizeof(ptrsKeys_L)/sizeof(*ptrsKeys_L);
-Row row_L(scanner_L, 16, ptrsKeys_L, KEY_COUNT_0); //strobe pin 0
+Key* ptrsKeys_L[] = { &s_X, &l_fn, &k_a };
+const uint8_t KEY_COUNT_L = sizeof(ptrsKeys_L)/sizeof(*ptrsKeys_L);
+Row row_L(scanner_L, 16, ptrsKeys_L, KEY_COUNT_L); //strobe pin 0
 
-Key* ptrsKeys_R[] = { &s_Y, &s_R, &s_7 };
-const uint8_t KEY_COUNT_1 = sizeof(ptrsKeys_R)/sizeof(*ptrsKeys_R);
-Row row_R(scanner_R, 17, ptrsKeys_R, KEY_COUNT_1); //strobe pin 1
+Key* ptrsKeys_R[] = { &s_Y, &s_B, &s_C };
+const uint8_t KEY_COUNT_R = sizeof(ptrsKeys_R)/sizeof(*ptrsKeys_R);
+Row row_R(scanner_R, 17, ptrsKeys_R, KEY_COUNT_R); //strobe pin 1
 
 // ################### MAIN ####################
 void setup()
